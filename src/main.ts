f23524f0b686f38cc6b6bc9ecb650d00855cc6ad
Thomas Schmitz <dev@tschmitz.de>
2026-05-01 22:09:08 +0200

    perf: lazy load cli use cases

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,12 @@
 #!/usr/bin/env node
-import { createBackupUseCases } from "./application/backup-use-cases.js";
-import { createChatUseCase } from "./application/chat-use-case.js";
-import { createConfigSchemaUseCase } from "./application/config-schema-use-case.js";
-import { createGetConfigValueUseCase } from "./application/get-config-value-use-case.js";
-import { createInitConfigUseCase } from "./application/init-config-use-case.js";
-import { createPluginUseCases } from "./application/plugin-use-cases.js";
-import { createReloadConfigUseCase } from "./application/reload-config-use-case.js";
-import { createRunDaemonUseCase, type RunDaemonOutcome } from "./application/run-daemon-use-case.js";
-import { createServiceUseCases } from "./application/service-use-cases.js";
-import { createSetConfigValueUseCase } from "./application/set-config-value-use-case.js";
-import { createSyncManagedSkillsUseCase } from "./application/sync-managed-skills-use-case.js";
-import { createValidateConfigUseCase } from "./application/validate-config-use-case.js";
-import { createViewLogsUseCase } from "./application/view-logs-use-case.js";
-import { createCli } from "./cli/create-cli.js";
+import type { RunDaemonOutcome } from "./application/run-daemon-use-case.js";
+import { createCli, type CliDependencies } from "./cli/create-cli.js";
 import {
   ConfigurationError,
   RuntimeStateError,
   TransportResolutionError,
   UnsupportedPlatformError,
 } from "./domain/errors.js";
-import { createDaemonStartupFailureReporter } from "./logging/daemon-startup-failure-reporter.js";
 
 const EXIT_CODES = {
   unknown: 1,
@@ -30,41 +17,7 @@ const EXIT_CODES = {
 } as const;
 
 async function main(): Promise<void> {
-  const serviceUseCases = createServiceUseCases();
-  const startupFailureReporter = createDaemonStartupFailureReporter();
-  const runDaemonUseCase = createRunDaemonUseCase({ startupFailureReporter });
-  const validateConfigUseCase = createValidateConfigUseCase();
-  const backupUseCases = createBackupUseCases();
-  const pluginUseCases = createPluginUseCases();
-  const cli = createCli({
-    startDaemon: async (options) => {
-      const outcome = await runDaemonUseCase(options);
-      presentRunDaemonOutcome(outcome);
-    },
-    startChat: createChatUseCase(),
-    viewLogs: createViewLogsUseCase(),
-    validateConfig: validateConfigUseCase,
-    showConfigSchema: createConfigSchemaUseCase(),
-    reloadConfig: createReloadConfigUseCase(),
-    getConfigValue: createGetConfigValueUseCase(),
-    setConfigValue: createSetConfigValueUseCase(),
-    initConfig: createInitConfigUseCase(),
-    syncManagedSkills: createSyncManagedSkillsUseCase(),
-    createBackup: backupUseCases.createBackup,
-    inspectBackup: backupUseCases.inspectBackup,
-    restoreBackup: backupUseCases.restoreBackup,
-    listPlugins: pluginUseCases.listPlugins,
-    inspectPlugin: pluginUseCases.inspectPlugin,
-    checkPlugin: pluginUseCases.doctorPlugin,
-    statusPlugin: pluginUseCases.statusPlugin,
-    installPlugin: pluginUseCases.installPlugin,
-    installService: serviceUseCases.installService,
-    uninstallService: serviceUseCases.uninstallService,
-    startService: serviceUseCases.startService,
-    stopService: serviceUseCases.stopService,
-    restartService: serviceUseCases.restartService,
-    statusService: serviceUseCases.statusService,
-  });
+  const cli = createCli(createLazyCliDependencies());
 
   if (process.argv.length <= 2) {
     cli.outputHelp();
@@ -72,6 +25,126 @@ async function main(): Promise<void> {
   }
 
   await cli.parseAsync(process.argv);
+}
+
+function createLazyCliDependencies(): CliDependencies {
+  const runDaemonUseCase = createLazyValue(async () => {
+    const [{ createRunDaemonUseCase }, { createDaemonStartupFailureReporter }] = await Promise.all([
+      import("./application/run-daemon-use-case.js"),
+      import("./logging/daemon-startup-failure-reporter.js"),
+    ]);
+    const startupFailureReporter = createDaemonStartupFailureReporter();
+    const runDaemon = createRunDaemonUseCase({ startupFailureReporter });
+
+    return async (options: Parameters<CliDependencies["startDaemon"]>[0]) => {
+      const outcome = await runDaemon(options);
+      presentRunDaemonOutcome(outcome);
+    };
+  });
+  const chatUseCase = createLazyUseCase(async () => (await import("./application/chat-use-case.js")).createChatUseCase());
+  const viewLogsUseCase = createLazyUseCase(
+    async () => (await import("./application/view-logs-use-case.js")).createViewLogsUseCase(),
+  );
+  const validateConfigUseCase = createLazyUseCase(
+    async () => (await import("./application/validate-config-use-case.js")).createValidateConfigUseCase(),
+  );
+  const showConfigSchemaUseCase = createLazyUseCase(
+    async () => (await import("./application/config-schema-use-case.js")).createConfigSchemaUseCase(),
+  );
+  const reloadConfigUseCase = createLazyUseCase(
+    async () => (await import("./application/reload-config-use-case.js")).createReloadConfigUseCase(),
+  );
+  const getConfigValueUseCase = createLazyUseCase(
+    async () => (await import("./application/get-config-value-use-case.js")).createGetConfigValueUseCase(),
+  );
+  const setConfigValueUseCase = createLazyUseCase(
+    async () => (await import("./application/set-config-value-use-case.js")).createSetConfigValueUseCase(),
+  );
+  const initConfigUseCase = createLazyUseCase(
+    async () => (await import("./application/init-config-use-case.js")).createInitConfigUseCase(),
+  );
+  const syncManagedSkillsUseCase = createLazyUseCase(
+    async () => (await import("./application/sync-managed-skills-use-case.js")).createSyncManagedSkillsUseCase(),
+  );
+  const backupUseCases = createLazyValue(async () => (await import("./application/backup-use-cases.js")).createBackupUseCases());
+  const pluginUseCases = createLazyValue(async () => (await import("./application/plugin-use-cases.js")).createPluginUseCases());
+  const serviceUseCases = createLazyValue(async () => (await import("./application/service-use-cases.js")).createServiceUseCases());
+
+  return {
+    startDaemon: async (options) => {
+      await (await runDaemonUseCase())(options);
+    },
+    startChat: chatUseCase,
+    viewLogs: viewLogsUseCase,
+    validateConfig: validateConfigUseCase,
+    showConfigSchema: showConfigSchemaUseCase,
+    reloadConfig: reloadConfigUseCase,
+    getConfigValue: getConfigValueUseCase,
+    setConfigValue: setConfigValueUseCase,
+    initConfig: initConfigUseCase,
+    syncManagedSkills: syncManagedSkillsUseCase,
+    createBackup: async (options) => {
+      await (await backupUseCases()).createBackup(options);
+    },
+    inspectBackup: async (options) => {
+      await (await backupUseCases()).inspectBackup(options);
+    },
+    restoreBackup: async (options) => {
+      await (await backupUseCases()).restoreBackup(options);
+    },
+    listPlugins: async (options) => {
+      await (await pluginUseCases()).listPlugins(options);
+    },
+    inspectPlugin: async (options) => {
+      await (await pluginUseCases()).inspectPlugin(options);
+    },
+    checkPlugin: async (options) => {
+      await (await pluginUseCases()).doctorPlugin(options);
+    },
+    statusPlugin: async (options) => {
+      await (await pluginUseCases()).statusPlugin(options);
+    },
+    installPlugin: async (options) => {
+      await (await pluginUseCases()).installPlugin(options);
+    },
+    installService: async (options) => {
+      await (await serviceUseCases()).installService(options);
+    },
+    uninstallService: async (options) => {
+      await (await serviceUseCases()).uninstallService(options);
+    },
+    startService: async (options) => {
+      await (await serviceUseCases()).startService(options);
+    },
+    stopService: async (options) => {
+      await (await serviceUseCases()).stopService(options);
+    },
+    restartService: async (options) => {
+      await (await serviceUseCases()).restartService(options);
+    },
+    statusService: async (options) => {
+      await (await serviceUseCases()).statusService(options);
+    },
+  };
+}
+
+function createLazyUseCase<TArgs extends unknown[]>(
+  createUseCase: () => Promise<(...args: TArgs) => Promise<void>>,
+): (...args: TArgs) => Promise<void> {
+  const lazyUseCase = createLazyValue(createUseCase);
+
+  return async (...args: TArgs) => {
+    await (await lazyUseCase())(...args);
+  };
+}
+
+function createLazyValue<TValue>(createValue: () => Promise<TValue>): () => Promise<TValue> {
+  let value: Promise<TValue> | undefined;
+
+  return () => {
+    value ??= createValue();
+    return value;
+  };
 }
 
 function presentRunDaemonOutcome(outcome: RunDaemonOutcome): void {
